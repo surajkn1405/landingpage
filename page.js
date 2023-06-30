668ccd8a5539fe1f3b3adad491b589e5343dcb91
Suraj Kumar Nayak <119889764+surajkn1405@users.noreply.github.com>
2023-06-30 10:37:01 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,22 @@
+
+var countDownDate = new Date("June 20, 2023 11:20:30").getTime();
+
+var x = setInterval(function(){
+
+    var now = new Date.getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance/ (1000 * 60 *60 * 24));
+    var days = Math.floor((distance% (1000 * 60 *60 * 24))/(1000 * 60 *60 ));
+    var days = Math.floor((distance% (1000 * 60 *60 ))/(1000 * 60 ));
+    var days = Math.floor((distance% (1000 * 60 ))/1000);
+
+    document.getElementById("demo").innerHTML = days +"d "+ hours +"h "+ minutes + "m "+ seconds + "s ";
+
+    if(distance<0){
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+
+}, 1000);
